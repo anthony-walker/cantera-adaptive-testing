91a39217443bbe65076ee1a98ca6915311ed994d
Anthony Walker <walkanth@oregonstate.edu>
2022-04-04 14:07:44 -0700

    Update Volume Problem

--- cantera_adaptive_testing/model_base.py
+++ cantera_adaptive_testing/model_base.py
@@ -154,7 +154,6 @@ class ModelBase(object):
             func(self)
             tf = time.time_ns()
             # post function analysis
-            print(self.sim_end_time)
             num_stats = self.get_numerical_stats()
             self.currRun[func.__name__].update({"simulation_info":{"runtime_seconds": round((tf-t0) * 1e-9, 8), "time_steps":self.ctr, "sim_end_time": self.sim_end_time, "date":self.currRunTime}})
             self.currRun[func.__name__].update(self.thermo_data)
@@ -221,6 +220,323 @@ class ModelBase(object):
 
     @problem
     def volume_problem(self):
+        """
+        This problem was adapted from
+        https://cantera.org/examples/python/reactors/ic_engine.py.html
+
+        Simulation of a (gaseous) Diesel-type internal combustion engine.
+
+        The simulation uses n-Dodecane as fuel, which is injected close to top dead
+        center. Note that this example uses numerous simplifying assumptions and
+        thus serves for illustration purposes only.
+
+        Requires: cantera >= 2.5.0, scipy >= 0.19, matplotlib >= 2.0
+        """
+
+        #########################################################################
+        # Input Parameters
+        #########################################################################
+        T0 = 300
+        P0 = 1600e5
+        f = 3000. / 60.  # engine speed [1/s] (3000 rpm)
+        V_H = .5e-3  # displaced volume [m**3]
+        epsilon = 20.  # compression ratio [-]
+        d_piston = 0.083  # piston diameter [m]
+        # turbocharger temperature, pressure, and composition
+        T_inlet = T0  # K
+        p_inlet = 1.3e5  # Pa
+        comp_inlet = self.air
+        # outlet pressure
+        p_outlet = 1.2e5  # Pa
+        # fuel properties (gaseous!)
+        T_injector = T0  # K
+        p_injector = P0  # Pa
+        comp_injector = self.fuel
+        # ambient properties
+        T_ambient = T0  # K
+        p_ambient = ct.one_atm  # Pa
+        comp_ambient = self.air
+        # Inlet valve friction coefficient, open and close timings
+        inlet_valve_coeff = 1.e-6
+        inlet_open = -18. / 180. * np.pi
+        inlet_close = 198. / 180. * np.pi
+        # Outlet valve friction coefficient, open and close timings
+        outlet_valve_coeff = 1.e-6
+        outlet_open = 522. / 180 * np.pi
+        outlet_close = 18. / 180. * np.pi
+        # Fuel mass, injector open and close timings
+        injector_open = 350. / 180. * np.pi
+        injector_close = 365. / 180. * np.pi
+        injector_mass = 3.2e-5  # kg
+        # Simulation time and parameters
+        sim_n_revolutions = 8
+        delta_T_max = 20.
+        rtol = 1.e-12
+        atol = 1.e-16
+
+        #####################################################################
+        # Set up IC engine Parameters and Functions
+        #####################################################################
+
+        V_oT = V_H / (epsilon - 1.)
+        A_piston = .25 * np.pi * d_piston ** 2
+        stroke = V_H / A_piston
+
+        def crank_angle(t):
+            """Convert time to crank angle"""
+            return np.remainder(2 * np.pi * f * t, 4 * np.pi)
+
+        def piston_speed(t):
+            """Approximate piston speed with sinusoidal velocity profile"""
+            return - stroke / 2 * 2 * np.pi * f * np.sin(crank_angle(t))
+
+        #####################################################################
+        # Set up Reactor Network
+        #####################################################################
+        # load reaction mechanism
+        gas = ct.Solution(self.model)
+        # define initial state and set up reactor
+        gas.TPX = T_inlet, p_inlet, comp_inlet
+        cyl = ct.IdealGasMoleReactor(gas)
+        cyl.volume = V_oT
+        # define inlet state
+        gas.TPX = T_inlet, p_inlet, comp_inlet
+        inlet = ct.Reservoir(gas)
+        # inlet valve
+        inlet_valve = ct.Valve(inlet, cyl)
+        inlet_delta = np.mod(inlet_close - inlet_open, 4 * np.pi)
+        inlet_valve.valve_coeff = inlet_valve_coeff
+        inlet_valve.set_time_function(lambda t: np.mod(crank_angle(t) - inlet_open, 4 * np.pi) < inlet_delta)
+        # define injector state (gaseous!)
+        gas.TPX = T_injector, p_injector, comp_injector
+        injector = ct.Reservoir(gas)
+        # injector is modeled as a mass flow controller
+        injector_mfc = ct.MassFlowController(injector, cyl)
+        injector_delta = np.mod(injector_close - injector_open, 4 * np.pi)
+        injector_t_open = (injector_close - injector_open) / 2. / np.pi / f
+        injector_mfc.mass_flow_coeff = injector_mass / injector_t_open
+        injector_mfc.set_time_function(lambda t: np.mod(crank_angle(t) - injector_open, 4 * np.pi) < injector_delta)
+        # define outlet pressure (temperature and composition don't matter)
+        gas.TPX = T_ambient, p_outlet, comp_ambient
+        outlet = ct.Reservoir(gas)
+        # outlet valve
+        outlet_valve = ct.Valve(cyl, outlet)
+        outlet_delta = np.mod(outlet_close - outlet_open, 4 * np.pi)
+        outlet_valve.valve_coeff = outlet_valve_coeff
+        outlet_valve.set_time_function(lambda t: np.mod(crank_angle(t) - outlet_open, 4 * np.pi) < outlet_delta)
+        # define ambient pressure (temperature and composition don't matter)
+        gas.TPX = T_ambient, p_ambient, comp_ambient
+        ambient_air = ct.Reservoir(gas)
+        # piston is modeled as a moving wall
+        piston = ct.Wall(ambient_air, cyl)
+        piston.area = A_piston
+        piston.set_velocity(piston_speed)
+        # create a reactor network containing the cylinder and limit advance step
+        self.net = ct.ReactorNet([cyl])
+        self.net.rtol, self.net.atol = rtol, atol
+        cyl.set_advance_limit('temperature', delta_T_max)
+        # apply numerical options
+        self.apply_numerical_options()
+        #####################################################################
+        # Run Simulation
+        #####################################################################
+        # set up output data arrays
+        if self.write:
+            states = ct.SolutionArray(cyl.thermo, extra=('t', 'ca', 'V', 'm', 'mdot_in', 'mdot_out', 'dWv_dt'),)
+        # simulate with a maximum resolution of 1 deg crank angle
+        dt = 1. / (360 * f)
+        t_stop = sim_n_revolutions / f
+        self.sim_end_time = 0
+        self.ctr = 0
+        while self.net.time < t_stop:
+            # perform time integration
+            try:
+                self.net.advance(self.net.time + dt)
+                self.ctr += 1
+            except Exception as e:
+                self.exception = {"exception": str(e)}
+                self.sim_end_time = self.net.time
+            if self.write:
+                # calculate results to be stored
+                dWv_dt = - (cyl.thermo.P - ambient_air.thermo.P) * A_piston * \
+                    piston_speed(self.net.time)
+                # append output data
+                states.append(cyl.thermo.state, t=self.net.time, ca=crank_angle(self.net.time), V=cyl.volume, m=cyl.mass, mdot_in=inlet_valve.mass_flow_rate, mdot_out=outlet_valve.mass_flow_rate, dWv_dt=dWv_dt)
+        # Run a loop over decreasing residence times, until the reactor is extinguished,
+        # saving the state after each iteration.
+        if self.write:
+            states.append(reactor.thermo.state)
+            csvName = self.runName + "-" + "volume" + ".csv"
+            csvName = os.path.join(self.dataDir, csvName)
+            states.write_csv(csvName, cols=('T', 'D', 'X'))
+            self.currRun.update({"writefile": csvName.split("/")[-1]})
+
+
+    @problem
+    def volume_problem_mixing(self):
+        """
+        This problem was adaptive from https://cantera.org/examples/python/reactors/mix1.py.html.
+
+        Mixing two streams.
+
+        Since reactors can have multiple inlets and outlets, they can be used to
+        implement mixers, splitters, etc. In this example, air and methane are mixed
+        in stoichiometric proportions. Due to the low temperature, no reactions occur.
+        Note that the air stream and the methane stream use *different* reaction
+        mechanisms, with different numbers of species and reactions. When gas flows
+        from one reactor or reservoir to another one with a different reaction
+        mechanism, species are matched by name. If the upstream reactor contains a
+        species that is not present in the downstream reaction mechanism, it will be
+        ignored. In general, reaction mechanisms for downstream reactors should
+        contain all species that might be present in any upstream reactor.
+
+        Compare this approach for the transient problem to the method used for the
+        steady-state problem in thermo/mixing.py.
+
+        Requires: cantera >= 2.5.0
+        """
+        # initial conditions
+        T0, P0 = 300.0, ct.one_atm
+        # Use air for stream a.
+        gas_a = ct.Solution('air.yaml')
+        gas_a.TPX = T0, P0, self.air
+        rho_a = gas_a.density
+        # Use Model for stream b (methane) and for the mixer. If it is desired
+        # to have a pure mixer, with no chemistry, use instead a reaction mechanism
+        # for gas_b that has no reactions.
+        gas_b = ct.Solution(self.model)
+        gas_b.TPX = T0, P0, self.fuel
+        rho_b = gas_b.density
+        # Create reservoirs for the two inlet streams and for the outlet stream.  The
+        # upsteam reservoirs could be replaced by reactors, which might themselves be
+        # connected to reactors further upstream. The outlet reservoir could be
+        # replaced with a reactor with no outlet, if it is desired to integrate the
+        # composition leaving the mixer in time, or by an arbitrary network of
+        # downstream reactors.
+        res_a = ct.Reservoir(gas_a)
+        res_b = ct.Reservoir(gas_b)
+        downstream = ct.Reservoir(gas_b)
+        # Create a reactor for the mixer. A reactor is required instead of a
+        # reservoir, since the state will change with time if the inlet mass flow
+        # rates change or if there is chemistry occurring.
+        gas_b.TPX = T0, P0, self.air
+        mixer = ct.IdealGasMoleReactor(gas_b)
+        # add properties to yaml
+        self.thermo_data.update({"thermo":{"model": self.model.split("/")[-1], "mole-reactor":self.moles, "nreactions":gas_b.n_reactions, "nspecies":gas_b.n_species, "fuel":self.fuel, "air": self.air, "equiv_ratio": -1, "T0":T0, "P0":P0, "V0":mixer.volume}})
+        # create two mass flow controllers connecting the upstream reservoirs to the
+        # mixer, and set their mass flow rates to values corresponding to
+        # stoichiometric combustion.
+        mfc1 = ct.MassFlowController(res_a, mixer, mdot=rho_a*2.5/0.21)
+        mfc2 = ct.MassFlowController(res_b, mixer, mdot=rho_b*1.0)
+        # connect the mixer to the downstream reservoir with a valve.
+        outlet = ct.Valve(mixer, downstream, K=10.0)
+        states = ct.SolutionArray(gas_b)
+        # create reactor network
+        self.net = ct.ReactorNet([mixer])
+        # apply numerical options
+        self.apply_numerical_options()
+        # Run a loop over decreasing residence times, until the reactor is extinguished,
+        # saving the state after each iteration.
+        try:
+            self.net.advance_to_steady_state()  # adv to ss
+            self.sim_end_time = self.net.time
+            self.ctr = -1
+        except Exception as e:
+            self.exception = {"exception": str(e)}
+        if self.write:
+            states.append(reactor.thermo.state)
+            csvName = self.runName + "-" + "volume" + ".csv"
+            csvName = os.path.join(self.dataDir, csvName)
+            states.write_csv(csvName, cols=('T', 'D', 'X'))
+            self.currRun.update({"writefile": csvName.split("/")[-1]})
+
+
+    @problem
+    def volume_problem_combustor(self):
+        """
+        An example modified from https://cantera.org/examples/python/reactors/combustor.py.html to be used with preconditioning:
+
+        A combustor, modeled as a single well-stirred reactor.
+
+        We are interested in the steady-state burning solution. This example explores
+        the effect of changing the residence time on completeness of reaction (through
+        the burned gas temperature) and on the total heat release rate.
+
+        Demonstrates the use of a MassFlowController where the mass flow rate function
+        depends on variables other than time by capturing these variables from the
+        enclosing scope. Also shows the use of a PressureController to create a constant
+        pressure reactor with a fixed volume.
+
+        Requires: cantera >= 2.5.0, matplotlib >= 2.0
+        """
+        # Use reaction mechanism GRI-Mech 3.0. For 0-D simulations,
+        # no transport model is necessary.
+        gas = ct.Solution(self.model)
+        # Create a Reservoir for the inlet, set to a methane/air mixture at a specified
+        # equivalence ratio
+        self.equiv_ratio = 1  # lean combustion
+        T0, P0 = 1600, ct.one_atm
+        gas.TP = T0, P0
+        gas.set_equivalence_ratio(self.equiv_ratio, self.fuel, self.air)
+        inlet = ct.Reservoir(gas)
+        # Create the combustor, and fill it initially with a mixture consisting of the
+        # equilibrium products of the inlet mixture. This state corresponds to the state
+        # the reactor would reach with infinite residence time, and thus provides a good
+        # initial condition from which to reach a steady-state solution on the reacting
+        # branch.
+        # gas.equilibrate('UV')
+        combustor = ct.IdealGasMoleReactor(gas)
+        combustor.volume = 1.0
+        # add properties to yaml
+        self.thermo_data.update({"thermo":{"model": self.model.split("/")[-1], "mole-reactor":self.moles, "nreactions":gas.n_reactions, "nspecies":gas.n_species, "fuel":self.fuel, "air": self.air, "equiv_ratio": self.equiv_ratio, "T0":T0, "P0":P0, "V0":combustor.volume}})
+        # Create a reservoir for the exhaust
+        exhaust = ct.Reservoir(gas)
+        # Use a variable mass flow rate to keep the residence time in the reactor
+        # constant (residence_time = mass / mass_flow_rate). The mass flow rate function
+        # can access variables defined in the calling scope, including state variables
+        # of the Reactor object (combustor) itself.
+
+        def mdot(t):
+            return combustor.mass / residence_time
+
+        inlet_mfc = ct.MassFlowController(inlet, combustor, mdot=mdot)
+        # A PressureController has a baseline mass flow rate matching the 'master'
+        # MassFlowController, with an additional pressure-dependent term. By explicitly
+        # including the upstream mass flow rate, the pressure is kept constant without
+        # needing to use a large value for 'K', which can introduce undesired stiffness.
+        outlet_mfc = ct.PressureController(combustor, exhaust, master=inlet_mfc, K=0.01)
+        # the simulation only contains one reactor
+        # Create the reactor network
+        self.net = ct.ReactorNet([combustor])
+        # apply numerical options
+        self.apply_numerical_options()
+        # Run a loop over decreasing residence times, until the reactor is extinguished,
+        # saving the state after each iteration.
+        states = ct.SolutionArray(gas, extra=['tres'])
+        residence_time = 0.1  # starting residence time
+        self.sim_end_time = 0.0
+        self.ctr = 0
+        tf = 1e-5
+        while combustor.T > 500:
+            # perform time integration
+            try:
+                self.net.set_initial_time(0.0)  # reset the integrator
+                self.net.advance_to_steady_state()
+                residence_time *= 0.9  # decrease the residence time for the next iteration
+                self.sim_end_time += self.net.time
+                self.ctr += 1
+            except Exception as e:
+                self.exception = {"exception": str(e)}
+            if self.write:
+                states.append(reactor.thermo.state, tres=self.sim_end_time)
+        if self.write:
+            csvName = self.runName + "-" + "volume" + ".csv"
+            csvName = os.path.join(self.dataDir, csvName)
+            states.write_csv(csvName, cols=('T', 'D', 'X', 'tres'))
+            self.currRun.update({"writefile": csvName.split("/")[-1]})
+
+    @problem
+    def volume_problem_fuel_injection(self):
         """
         This problem was adapted from
         https://cantera.org/examples/python/reactors/fuel_injection.py.html
