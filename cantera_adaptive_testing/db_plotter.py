ea4cb4d454f5a732c3c867bca617bd24ef62ea4c
Anthony Walker <walkerant@kairospower.com>
2022-11-06 07:03:11 -0600

    Working on plotting

--- cantera_adaptive_testing/db_plotter.py
+++ cantera_adaptive_testing/db_plotter.py
@@ -1,5 +1,6 @@
 import sqlite3
 import matplotlib.pyplot as plt
+from cantera_adaptive_testing.database_utils import *
 
 # use appropriate backend
 # change font
@@ -8,7 +9,31 @@ plt.rcParams['mathtext.rm'] = 'serif'
 plt.rcParams["font.family"] = 'serif'
 # getting yaml for use in functions
 
-def plot_contour(database, args, **kwargs):
-    pass
+def plot_contour_threshold_time(database, model_name, problem, prop, **kwargs):
+    # create record names
+    names = []
+    classifiers = []
+    if kwargs.pop("remove_thirdbody", False):
+        classifiers.append("ntb")
+    if kwargs.pop("remove_falloff", False):
+        classifiers.append("nfo")
+    classifiers.append(problem)
+    for i in range(0, 19):
+        names.append("_".join([model_name, str(i)]+classifiers))
+    # get database of interest
+    connect = get_database_connection(database)
+    cursor = connect.cursor()
+    cursor.execute("SELECT * FROM dbname.sqlite_master WHERE type='table'")
+    names = cursor.fetchall()
+    print(names)
+    # query each table for the condition of interest
+    # for n in names:
+    #     query = f"SELECT {prop} FROM "+"{:s}"
+    #     cursor.execute(query.format(n))
+    #     res = cursor.fetchall()
+    #     print(res)
+    #     input()
 
 
+if __name__ == "__main__":
+    plot_contour_threshold_time("surf-perf-temp.db", "PlatinumSmallHydrogen", "plug_flow_reactor", "condition")
