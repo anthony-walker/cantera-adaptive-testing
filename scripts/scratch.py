882912ab5e7a2d918ddfda5a83c7929fd0fbb17c
Anthony Walker <walkerant@kairospower.com>
2022-11-16 14:20:04 -0500

    Remove symposium data and manage other data

--- scripts/scratch.py
+++ scripts/scratch.py
@@ -8,36 +8,28 @@ files = os.listdir("surf-data")
 files = list(filter(lambda x: ".yaml" in x, files))
 mods, ___ = zip(*inspect.getmembers(models, inspect.isclass))
 mods = list(filter(lambda x: "Platinum" in x, mods))
-# combine all mod files
-for m in mods:
-    print(m)
-    curr_files = list(filter(lambda x: m in x, files))
-    print(len(curr_files))
-# check if model already exists
-# if not os.path.isfile(self.model):
-#     # read in model
-#     with open(model, 'r') as f:
-#         data = yaml.load(f)
-#     # remove appropriate reactions
-#     reactions = []
-#     for k in data["reactions"]:
-#         if "type" in k.keys():
-#             if k["type"] == "falloff" and self.remove_falloff:
-#                 if self.replace_reactions:
-#                     k.pop("type", None)
-#                     k.pop("Troe", None)
-#                     k.pop("high-P-rate-constant", None)
-#                     k.pop("efficiencies", None)
-#                     k["rate-constant"] = k.pop("low-P-rate-constant")
-#                     reactions.append(k)
-#             elif k["type"] == "three-body" and self.remove_thirdbody:
-#                 if self.replace_reactions:
-#                     k.pop("type", None)
-#                     k.pop("efficiencies", None)
-#                     reactions.append(k)
-#             else:
-#                 reactions.append(k)
-#     # write new file
+
+# Had to add nruns to some of the initial runs
+if 0:
+    files = list(filter(lambda x: "Large" not in x, files))
+    for fi in files:
+        print(fi)
+        with open(os.path.join("surf-data", fi), "r") as f:
+            data = yaml.load(f)
+        for m in data:
+            for p in data[m]:
+                data[m][p]["nruns"] = 1
+        with open(os.path.join("surf-data", fi), "w") as f:
+            yaml.dump(data, f)
+
+#
+# for m in mods[:1]:
+#     curr_files = list(filter(lambda x: m in x, files))[:2]
+#     for file in curr_files:
+#         with open(curr_files, "r") as f:
+#             data = yaml.load(f)
+#         fkey = data.keys()[0]
+#         print(fkey)
 #     data["reactions"] = ruamel.yaml.comments.CommentedSeq(reactions)
 #     with open(self.model, 'w') as f:
 #         data = yaml.dump(data, f)
--- scripts/surf-study.sh
+++ scripts/surf-study.sh
@@ -10,10 +10,32 @@ skip_approx
 
 ./launch.sh ./options/surf-opts single 1 1 $MLIST -R steady -MTS 1e-3 -MS 1e9
 
+./launch.sh ./options/surf-opts single 1 1 $MLIST -R steady -MTS 1e-3 -MS 1e9 --remove_falloff
+
+./launch.sh ./options/surf-opts single 1 1 $MLIST -R steady -MTS 1e-3 -MS 1e9 --remove_thirdbody
+
+./launch.sh ./options/surf-opts single 1 1 $MLIST -R steady -MTS 1e-3 -MS 1e9 --remove_falloff --remove_thirdbody
+
 reset_skips
 skip_moles
 skip_analyt
 
+# normal runs
 ./launch.sh ./options/surf-opts single 1 1 $MLIST -R analysis -D "surf-data.db"
 
 ./launch.sh ./options/surf-opts mpi 10 1 $MLIST -R performance
+
+# remove falloff runs
+./launch.sh ./options/surf-opts single 1 1 $MLIST -R analysis -D "surf-data.db" --remove_falloff
+
+./launch.sh ./options/surf-opts mpi 10 1 $MLIST -R performance --remove_falloff
+
+# remove thirdbody runs
+./launch.sh ./options/surf-opts single 1 1 $MLIST -R analysis -D "surf-data.db" --remove_thirdbody
+
+./launch.sh ./options/surf-opts mpi 10 1 $MLIST -R performance --remove_thirdbody
+
+# remove both runs
+./launch.sh ./options/surf-opts single 1 1 $MLIST -R analysis -D "surf-data.db" --remove_thirdbody --remove_falloff
+
+./launch.sh ./options/surf-opts mpi 10 1 $MLIST -R performance --remove_thirdbody --remove_falloff
