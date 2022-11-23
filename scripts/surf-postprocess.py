302b6e6a1c681ae499e755facf41786055c2de5a
Anthony Walker <walkanth@oregonstate.edu>
2022-11-23 09:02:42 -0800

    Working on updated results

--- scripts/surf-postprocess.py
+++ scripts/surf-postprocess.py
@@ -24,37 +24,25 @@ def add_nruns_to_older_files():
                 yaml.dump(data, f)
 
 
-def combine_surf_yamls():
+def combine_surf_yamls(direc="surface_data"):
     yaml = ruamel.yaml.YAML()
-    files = os.listdir("surf-data")
+    files = os.listdir(direc)
     files = list(filter(lambda x: ".yaml" in x, files))
     mods, ___ = zip(*inspect.getmembers(models, inspect.isclass))
     mods = list(filter(lambda x: "Platinum" in x, mods))
-    # Sort all files into cases
-    fp = lambda x: os.path.join("surf-data", x)
+    # sort all files into cases
+    fp = lambda x: os.path.join(direc, x)
     cases = {}
     for m in mods:
         curr_files = list(filter(lambda x: m in x, files))
         for cf in curr_files:
-            with open(fp(cf), "r") as f:
-                data = yaml.load(f)
-            for k1, v1 in data.items():
-                for k2 in v1.keys():
-                    if "config" in v1[k2]:
-                        config = dict(v1[k2]["config"])
-                        curr_key = m
-                        ths = config["threshold"]
-                        curr_key += f"-{ths:0.0e}" if config["preconditioned"] else "-mass"
-                        curr_key += "-ntb" if config["remove_thirdbody"] else ""
-                        curr_key += "-nfo" if config["remove_falloff"] else ""
-                        if curr_key in cases and config["runtype"] == "performance":
-                            cases[curr_key].append(cf)
-                            break
-                        elif config["runtype"] != "performance":
-                            cases[curr_key] = [cf]
-                            break
-    case_data = {}
+            case_key = "-".join(cf.split("-")[:-1])
+            if case_key in cases:
+                cases[case_key].append(cf)
+            else:
+                cases[case_key] = [cf]
     # combine cases
+    case_data = {}
     for case in cases:
         case_files = cases[case]
         case_data[case] = {}
@@ -63,9 +51,12 @@ def combine_surf_yamls():
                 data = yaml.load(f)
             for k1, v1 in data.items():
                 for k2, v2 in v1.items():
-                    if k2 not in case_data[case] and 'runtime' in v1[k2] and v2["config"]["runtype"] == "performance":
+                    # do nothing if exception is a key
+                    if "exception" in v1[k2]:
+                        pass
+                    elif k2 not in case_data[case]:
                         case_data[case][k2] = v1[k2]
-                    elif "runtime" in v2 and v2["config"]["runtype"] == "performance":
+                    else:
                         case_data[case][k2]["runtime"] += v2["runtime"]
                         case_data[case][k2]["nruns"] += 1
     # average the run times
@@ -78,4 +69,5 @@ def combine_surf_yamls():
 def plot_data_one():
     pass
 
-plot_data_one()
+# plot_data_one()
+combine_surf_yamls()
