88f3bbb3c66221a5f29b615122d70d4c43bbcb76
Anthony Walker <walkerant@kairospower.com>
2023-01-30 15:32:00 -0500

    Update to conditions

--- scripts/PR-scripts/temp.py
+++ scripts/PR-scripts/temp.py
@@ -1,31 +1,28 @@
 import re
+import ruamel.yaml
 
 with open("bc.txt") as f:
     lines = f.read()
 lines.strip()
 lines = list(filter(None, lines.split("\n")))
 lines.sort()
-lines = [lines[i:i+2] for i in range(0, len(lines), 2)]
-lns = []
-for l1, l2 in lines:
-    print(l1, l2)
-    name, prob, bran, t1, _, nspec = l1.split(" ")
-    name, prob, bran, t2, _, nspec = l2.split(" ")
-    t1 =  float(t1)
-    t2 = float(t2)
-    bw = t1 - t2
-    name = name[:10] if len(name) >= 10 else name + (10 - len(name)) * " "
-    prob = prob[:10] if len(prob) >= 10 else prob + (10 - len(prob)) * " "
-    nspec = nspec[:4] if len(nspec) >= 4 else nspec + (4 - len(nspec)) * " "
-    ostr = f"{name} {prob} {nspec} {t1:3.3f}    |    {t2:3.3f}    {bw:3.3f}\n"
-    lns.append(ostr)
+lines = [re.sub(":", "", l) for l in lines]
+data = {}
 
-with open("comp.txt", "w") as f:
-    f.write("Model     problem     nspecies   main    |    sdev    main - sdev\n")
-    f.write("IdealGasMoleReactor\n")
-    for l in filter(lambda x: "plug" not in x, lns):
-        f.write(l)
-    f.write("\n")
-    f.write("IdealGasConstPressureMoleReactor\n")
-    for l in filter(lambda x: "well" not in x, lns):
-        f.write(l)
+for l in lines:
+    name, prob, bran, t1, _, nspec = l.split(" ")
+    if name not in data:
+        data[name] = {"species": nspec}
+    if prob not in data[name]:
+        data[name][prob] = {}
+    if bran not in data[name][prob]:
+        data[name][prob][bran] = float(t1)
+
+for k in data:
+    for p in data[k]:
+        if p != "species":
+            data[k][p]["main-sdev"] = data[k][p].get("main", 0) - data[k][p].get("sdev", 0)
+
+yaml = ruamel.yaml.YAML()
+with open("comp.yaml", 'w') as f:
+    yaml.dump(data, f)
