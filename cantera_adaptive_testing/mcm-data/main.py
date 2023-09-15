57550d4322e82cabd254b2f14ed56110c6d63cf9
Anthony Walker <walkerant@kairospower.com>
2023-09-15 10:42:09 -0400

    Construction of mechanism pretty well complete

--- cantera_adaptive_testing/mcm-data/main.py
+++ cantera_adaptive_testing/mcm-data/main.py
@@ -1,6 +1,11 @@
+import re
 import yaml
+import time
+import string
+import random
 import datetime
 
+# a list of all chemical elements
 chemical_elements = [
     "H", "He", "Li", "Be", "B", "C", "N", "O", "F", "Ne",
     "Na", "Mg", "Al", "Si", "P", "S", "Cl", "Ar", "K", "Ca",
@@ -49,18 +54,46 @@ def main():
 
     # get all species names
     species_names, species_items = zip(*species_data.items())
+    # get all elements
+    complete_comp = {}
+    for si in species_items:
+        complete_comp.update(si["composition"])
+    elements = list(complete_comp.keys())
+    elements.sort()
     # atmosphere phase modification TODO: FIX elements
-    aerosol_data["phases"]["name"=="atmosphere"]["species"] = species_names
-    aerosol_data["phases"]["name"=="atmosphere"]["elements"] = chemical_elements
+    aerosol_data["phases"][0]["species"] = species_names
+    aerosol_data["phases"][0]["elements"] = elements
     # aerosol phase modification TODO: ALL
-
-    # TODO: Format unruly lists like elements and species in phases to make more readable.
+    aerosol_data["phases"][1]["species"] = ["NA", "SA"]
+    aerosol_data["phases"][1]["elements"] = ["H", "N", "O", "S"]
     # transfer data
     aerosol_data["species"] = species_items
     aerosol_data["atmosphere-reactions"] = reaction_data["atmosphere-reactions"]
     aerosol_data["aerosol-reactions"] = reaction_data["aerosol-reactions"]
     with open("aerosol.yaml", "w") as f:
         yaml.safe_dump(aerosol_data, f, default_flow_style=False, sort_keys=False)
+    # Format unruly lists like elements and species in phases to make more readable.
+    with open("aerosol.yaml", "r") as f:
+        content = f.read()
+        content = re.sub(r"\n  [-] ", ", ", content)
+        content = re.sub(r"[:][,]", ":", content)
+        content = re.sub(r"[']NO[']", "NO", content)
+        # search for list
+        random.seed(time.time())
+        rep_str = "".join(random.choices(string.ascii_letters, k=30))
+        res = re.search(r"(([A-Z]+[a-z]*[0-9]*)+[,][ ])+[^\n]*", content)
+        replacements = []
+        while res:
+            replacements.append((rep_str, res.group(0)))
+            content = content.replace(res.group(0), rep_str)
+            rep_str = "".join(random.choices(string.ascii_letters, k=30))
+            res = re.search(r"(([A-Z]+[a-z]*[0-9]*)+[,][ ])+[^\n]*", content)
+        for rep, orig in replacements:
+            orig = f"[{orig}]"
+            content = content.replace(rep, orig)
+    # rewrite content
+    with open("aerosol.yaml", "w") as f:
+        f.write(content)
 
 if __name__ == "__main__":
     main()
